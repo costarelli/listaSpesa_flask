b3ca7b33f6145f36755764f8e6f2b7604ba035ec
costarelli marco <costarelli.marco@iisgalvanimi.edu.it>
2024-11-27 09:41:49 +0000

    secondo commit

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 #inizializza l'app Flask
 app = Flask(__name__)
 #rotta principale
 @app.route('/')
 def home():
-    return "Per ora funziona tutto"
+    return render_template('index.html')
 #avvio dell'app Flask
 if __name__ == '__main__':
-    app.run(debug=True)git
+    app.run(debug=True)

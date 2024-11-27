c16d13f938b972df42ff376c8aee767b6b1e9df0
costarelli marco <costarelli.marco@iisgalvanimi.edu.it>
2024-11-27 09:58:36 +0000

    terzo commit

--- app.py
+++ app.py
@@ -2,9 +2,19 @@ from flask import Flask, render_template
 #inizializza l'app Flask
 app = Flask(__name__)
 #rotta principale
+lista_spesa=[]
 @app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html',lista=lista_spesa)
 #avvio dell'app Flask
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route('/aggiungi', methods=['POST'])
+def aggiungi():
+#ottiene elemento dal form
+    elemento = request.form['elemento']
+#aggiunge alla lista
+    if elemento:
+        lista_spesa.append(elemento)
+    return redirect(url_for('home'))
